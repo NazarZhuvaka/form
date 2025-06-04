da10a6cc5ef583695a5bb2e0341d2b05f1f4470a
Nazar <nazar.zhuvaka.mcboss@gmail.com>
2025-06-04 21:29:56 +0200

    Add vilidation

---
+++ assets/index.js
@@ -0,0 +1,37 @@
+const btn = document.querySelector(".form-btn");
+const form = document.querySelector("#myform");
+
+form.addEventListener("submit", sumbitHandler);
+
+function sumbitHandler(e) {
+  e.preventDefault();
+  const { target } = e;
+
+  const firstName = target
+    .querySelector('input[name="first-name"]')
+    .value.trim();
+  const lastName = target.querySelector('input[name="last-name"]').value.trim();
+  const email = target.querySelector('input[name="email"]').value.trim();
+
+  const phoneInputs = target.querySelectorAll('input[name="phone"]');
+  const phone = "+" +  Array.from(phoneInputs)
+    .map((input) => input.value.trim())
+    .join("");
+
+  const subject = target.querySelector('select[name="select"]').value;
+
+  const textarea = target.querySelector("textarea");
+  let message = textarea.value;
+  message = message.trim().replace(/\s+/g, " ");
+
+  const data = {
+    name: `${firstName} ${lastName}`.trim(),
+    email,
+    phone,
+    subject,
+    message,
+  };
+
+  console.log(data);
+  return data
+}
